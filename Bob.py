41d81bc191dc68fd9daff0256722aeff7c091053
[Aman Thakur] <unknnown1111@gmail.com>
2025-11-17 10:32:45 +0530

    add is_even() function

--- Bob.py
+++ Bob.py
@@ -9,3 +9,11 @@ def sum_up(a,b):
     return a+b
 
 print(sum_up(12,34))
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    return False
+
+print(is_even(10))  # True
+print(is_even(7))   # False
